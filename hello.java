4b0ba8d3b097b1c92676de8e5ecf40aa83cefcd4
BaMinhIT <90315099+BaMinhIT@users.noreply.github.com>
2021-09-08 18:31:32 +0700

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,10 @@
+/**
+ * hello
+ */
+public class hello {
+    public static void main(String[] args) {
+        String s = "nice to meet you";
+        System.out.println("toi la Minh");
+        System.out.println(s);
+    }
+}
